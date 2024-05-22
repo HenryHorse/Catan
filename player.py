77aac4aa3647333087390e6038009fb13f7ff536
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2024-05-22 10:03:36 -0700

    added settlement, city, road class

--- player.py
+++ player.py
@@ -1,3 +1,27 @@
+from catan import *
+
+class Game:
+    def __init__(self):
+        self.occ_tiles = [] #list of occupied road vertices
+        self.occ_roads = [] #list of occupied road vertices
+
+class Settlement:
+    def __init__(self, color, location):
+        self.color = color
+        self.location = location
+
+class City:
+    def __init__(self, color, location):
+        self.color = color
+        self.location = location
+
+class Road:
+    def __init__(self, color, rv1, rv2):
+        # road connects road vertex to road vertex
+        self.color = color
+        self.rv1 = rv1
+        self.rv2 = rv2
+
 class Player:
     def __init__(self, color):
         self.color = color
@@ -33,21 +57,30 @@ class Player:
     def remove_dev_card(self, dev_card):
         self.dev_cards.remove(dev_card)
 
-    def build_settlement(self, settlement):
+    def build_settlement(self, location):
         # can only build settlement if unbuilt settlements > 0
-        self.unbuilt_settlements -= 1
+        settlement = Settlement(self.color, location)
         self.settlements.append(settlement)
+        self.unbuilt_settlements -= 1
         self.victory_points += 1
 
-    def build_city(self, city):
+    def build_city(self, location):
         # can only build city if unbuilt cities > 0
-        self.unbuilt_cities -= 1
+        city = City(self.color, location)
         self.cities.append(city)
+        self.unbuilt_cities -= 1
         self.victory_points += 2
 
-    def build_road(self, road):
-        self.unbuilt_roads -= 1
+    def build_road(self, loc1, loc2):
+        road = Road(self.color, loc1, loc2)
         self.roads.append(road)
+        self.unbuilt_roads -= 1
 
     def get_victory_points(self):
         return self.victory_points
+
+def turn():
+    # roll dice
+    # trade
+    # build
+    pass
