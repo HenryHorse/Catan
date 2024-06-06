daf469de4e4ee32e09e6f4974cd5de91338af151
Kasey Liu <55379287+kaseyliu@users.noreply.github.com>
2024-06-05 22:26:52 -0700

    implemented basic building and trading for a turn, split up into game.py and turn.py

--- game.py
+++ game.py
@@ -1,11 +1,5 @@
 from catan import *
 import random
-
-class Game:
-    def __init__(self):
-        self.occ_tiles = [] #list of occupied road vertices
-        self.occ_roads = [] #list of occupied road vertices
-
 class Settlement:
     def __init__(self, color, location):
         self.color = color
@@ -22,12 +16,43 @@ class Road:
         self.color = color
         self.rv1 = rv1
         self.rv2 = rv2
+        
+class Game:
+    ''' keep track of the state of the game'''
+    def __init__(self):
+        self.occ_tiles = []  # occupied tile vertices
+        self.occ_roads = []  # occupied road vertices
+        self.players = []  # players in the game
+        self.tile_vertices = []  # all tile vertices on the board
+        self.road_vertices = []  # all road vertices on the board
 
+    def add_player(self, player):
+        self.players.append(player)
+
+    def get_tile_vertices(self):
+        return self.tile_vertices
+
+    def get_road_vertices(self):
+        return self.road_vertices
+
+    def is_valid_settlement_location(self, location):
+        return location not in self.occ_tiles
+
+    def is_valid_road_location(self, loc1, loc2):
+        # check if a road can be built between loc1 and loc2
+        return (loc1, loc2) not in self.occ_roads and (loc2, loc1) not in self.occ_roads
+
+    def occupy_tile(self, location):
+        self.occ_tiles.append(location)
+
+    def occupy_road(self, loc1, loc2):
+        self.occ_roads.append((loc1, loc2))
+        
 class Player:
     def __init__(self, color):
         self.color = color
         self.resources = {
-            'forest': 0,
+            'wood': 0,
             'grain': 0,
             'sheep': 0,
             'ore': 0,
@@ -79,59 +104,3 @@ class Player:
 
     def get_victory_points(self):
         return self.victory_points
-
-def roll_dice(n):
-    '''rolls dice n times and adds them'''
-    total = 0
-    for i in range(n):
-        total += random.randint(1, 6)
-    return total
-
-' -------------------- Turn --------------------'
-
-def turn_roll_dice():
-    tile_number = roll_dice(2)
-    # if the RoadVertex correspondng to settlement.location has tile w tile_number in the adjacent tiles, then give resources to the player.
-    my_settlement_locs = {}
-    for s in self.settlements:
-        my_settlement_locs.add(s.location)
-    for tile in tile_vertices:
-        if tile.number == tile_number and (tile.x, tile.y) in my_settlement_locs:
-            self.resources[tile.resource] += tile.number
-
-def turn_trade():
-    pass
-def turn_build():
-    pass
-
-def turn(player, tile_vertices, road_vertices):
-    if player.victory_points == 10:
-        return True
-    else:
-        # 1) resource production/roll dice
-        turn_roll_dice()
-        # 2) trade
-
-        # 3) build
-
-def main():
-    player_red = Player('red')
-    player_blue = Player('blue')
-
-    build_loc = (261.4359353944899, 400.0)
-    if unbuilt_settlements > 0:
-        build_settlement(build_loc)
-
-    # testing stuff
-    tile_vertices = get_tile_vertices()
-    road_vertices = get_road_vertices()
-    for tile in tile_vertices:
-        print("center number: ", tile.number)
-        print("\n(x,y): ", tile.x, tile.y)
-    print("\n\ntile vertices:", tile_vertices, "list size", len(tile_vertices))
-    print("\n\nroad vertices:", road_vertices, "list size", len(road_vertices))
-
-
-
-if __name__ == '__main__':
-    main()
---
+++ turn.py
@@ -0,0 +1,129 @@
+from catan import *
+from game import *
+
+def main():
+    player_red = Player('red')
+    player_blue = Player('blue')
+
+    game = Game()
+    tile_vertices = get_tile_vertices()  # Define this function
+    road_vertices = get_road_vertices()  # Define this function
+
+    while not turn(player_red, tile_vertices, road_vertices, game):
+        pass
+    print("Red player wins!")
+
+def turn(player, tile_vertices, road_vertices, game):
+    if player.victory_points >= 10:
+        return True
+    else:
+        # 1) resource production/roll dice
+        turn_roll_dice()
+        # 2) trade
+        turn_trade(player)
+        # 3) build
+        turn_build(player, game)
+
+def turn_roll_dice():
+    ''' gives player resources they own settlements on from the dice roll'''
+    tile_number = roll_dice(2)
+    # if the RoadVertex correspondng to settlement.location has tile w tile_number in the adjacent tiles, give resources to the player
+    my_settlement_locs = {}
+    for s in self.settlements:
+        my_settlement_locs.add(s.location)
+    for tile in tile_vertices:
+        if tile.number == tile_number and (tile.x, tile.y) in my_settlement_locs:
+            self.resources[tile.resource] += tile.number
+
+def turn_trade(player):
+    ''' trade any excess resources for needed ones'''
+    needed_resources = {'brick': 0, 'wood': 0, 'grain': 0, 'sheep': 0, 'ore': 0}
+    
+    # check what player can build and set needed resources
+    #building a settlement requires 1 brick, 1 wood, 1 grain, 1 sheep
+    if can_build_settlement(player):
+        needed_resources['brick'] += 1
+        needed_resources['wood'] += 1
+        needed_resources['grain'] += 1
+        needed_resources['sheep'] += 1
+    # building a road requires 1 brick and 1 wood
+    elif can_build_road(player): 
+        needed_resources['brick'] += 1
+        needed_resources['wood'] += 1
+    #building a city requires 3 grain and 2 ore
+    elif can_build_city(player):
+        needed_resources['grain'] += 2
+        needed_resources['ore'] += 3
+    
+    # TODO: add more advanced game logic
+        if player.resources[resource] < amount:
+            # TODO: implement trade logic
+            print(f"{player.color} needs {amount - player.resources[resource]} {resource}")
+
+def turn_build(player, game):
+    ''' build the first thing the player can (for now, change later?)'''
+    if can_build_settlement(player):
+        location = find_settlement_location(game)
+        player.build_settlement(location)
+    elif can_build_road(player):
+        loc1, loc2 = find_road_location(game)
+        player.build_road(loc1, loc2)
+    elif can_build_city(player):
+        location = find_city_location(game)
+        player.build_city(location)
+
+def roll_dice(n):
+    '''rolls dice n times and adds them'''
+    total = 0
+    for i in range(n):
+        total += random.randint(1, 6)
+    return total
+
+def can_build_settlement(player):
+    return (player.resources['brick'] >= 1 and 
+            player.resources['wood'] >= 1 and 
+            player.resources['grain'] >= 1 and 
+            player.resources['sheep'] >= 1 and 
+            player.unbuilt_settlements > 0)
+
+def can_build_road(player):
+    return (player.resources['brick'] >= 1 and 
+            player.resources['wood'] >= 1 and 
+            player.unbuilt_roads > 0)
+
+def can_build_city(player):
+    return (player.resources['grain'] >= 2 and 
+            player.resources['ore'] >= 3 and 
+            player.unbuilt_cities > 0)
+
+def find_settlement_location(game):
+    # TODO: find valid settlement location, set to 0,0 for now
+    return (0, 0)
+
+def find_road_location(game):
+    # TODO: find valid road location, set to 0,0 for now
+    return (0, 0), (0, 1)  
+
+def find_city_location(game):
+    # TODO: find valid city location, set to 0,0 for now
+    return (0, 0)
+
+# def main():
+#     player_red = Player('red')
+#     player_blue = Player('blue')
+
+#     build_loc = (261.4359353944899, 400.0)
+#     if unbuilt_settlements > 0:
+#         build_settlement(build_loc)
+
+#     # testing stuff
+#     # tile_vertices = get_tile_vertices()
+#     # road_vertices = get_road_vertices()
+#     # for tile in tile_vertices:
+#     #     print("center number: ", tile.number)
+#     #     print("\n(x,y): ", tile.x, tile.y)
+#     # print("\n\ntile vertices:", tile_vertices, "list size", len(tile_vertices))
+#     # print("\n\nroad vertices:", road_vertices, "list size", len(road_vertices))
+
+if __name__ == '__main__':
+    main()
